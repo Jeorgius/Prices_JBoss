a0608e9f46c3eaab1e44634d28c686501514efa3
Georgii Zykov <gzykov@lt-m.ru>
2019-01-14 10:19:56 +0300

    Implementing EJB

--- back/src/main/java/com/jeorgius/services/PriceService.java
+++ back/src/main/java/com/jeorgius/services/PriceService.java
@@ -1,4 +1,25 @@
 package com.jeorgius.services;
 
-public class PriceService implements IPricesgit  {
+import com.jeorgius.ejb.PriceEJB;
+
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import java.util.List;
+
+@Singleton
+public class PriceService implements IPrices  {
+
+    @EJB
+    private PriceEJB priceEJB;
+
+    @Override
+    public List<String> exportPrices(String product_code) {
+
+        return null;
+    }
+
+    @Override
+    public void importPrices(List<Long> priceValues) {
+
+    }
 }
--- view/src/main/java/com/jeorgius/rest/Rest.java
+++ view/src/main/java/com/jeorgius/rest/Rest.java
@@ -1,14 +1,7 @@
 package com.jeorgius.rest;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Context;
-import java.io.IOException;
 
 @ApplicationPath("api")
 public class Rest extends Application {}
